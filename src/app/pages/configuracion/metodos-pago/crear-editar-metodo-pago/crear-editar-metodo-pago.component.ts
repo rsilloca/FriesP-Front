2cd4f6508ec994b1819b28c9e2a4eadf0f2cddcf
Raquel Silloca <rsilloca@unsa.edu.pe>
2021-07-19 23:00:40 -0500

    Finally changes

--- src/app/pages/configuracion/metodos-pago/crear-editar-metodo-pago/crear-editar-metodo-pago.component.ts
+++ src/app/pages/configuracion/metodos-pago/crear-editar-metodo-pago/crear-editar-metodo-pago.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'fury-crear-editar-metodo-pago',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearEditarMetodoPagoComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  pasarelas = [
+    { id: 1, nombre: 'Niubiz' },
+    { id: 2, nombre: 'Stripe' },
+    { id: 3, nombre: 'Mercado Pago' }
+  ];
+  tipoDef: number;
+  editar: boolean = false;
+  tiposMetodoPago: any[] = [
+    { id: 1, nombre: 'Contra entrega' },
+    { id: 2, nombre: 'Transferencia bancaria' },
+    { id: 3, nombre: 'Pago por aplicación' },
+    { id: 4, nombre: 'Pasarela de Pago' },
+  ];
+
+  constructor(private fb: FormBuilder, public dialogRef: MatDialogRef<CrearEditarMetodoPagoComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
+    if (this.data) {
+      console.log(this.data);
+      this.editar = true;
+      this.tipoDef = this.tiposMetodoPago.find(t => t.nombre == this.data.tipo)?.id;
+    }
+    this.form = this.fb.group({
+      tipo: [this.tipoDef],
+      entidad: [],
+      cc: [],
+      cci: [],
+      app: [],
+      nroCelular: [],
+      pasarela: [],
+      token: []
+    });
   }
 
 }
--- src/app/pages/configuracion/metodos-pago/metodos-pago.component.ts
+++ src/app/pages/configuracion/metodos-pago/metodos-pago.component.ts
@@ -9,18 +9,25 @@ import { CrearEditarMetodoPagoComponent } from './crear-editar-metodo-pago/crear
 })
 export class MetodosPagoComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  tiposMetodoPago = [
+    { id: 1, nombre: 'Contra entrega' },
+    { id: 2, nombre: 'Transferencia bancaria' },
+    { id: 3, nombre: 'Pago por aplicación' },
+    { id: 4, nombre: 'Pasarela de Pago' },
+  ];
+
+  estados = [
+    { id: 1, nombre: 'ACTIVO' },
+    { id: 2, nombre: 'INACTIVO' }
+  ];
+
+  displayedColumns: string[] = ['N°', 'Nombre', 'Tipo', 'Estado', 'Acciones'];
   dataSource = [
-    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+    { nombre: 'Contra entrega', tipo: 'Contra entrega', estado: 'ACTIVO' },
+    { nombre: 'BCP', tipo: 'Transferencia bancaria', estado: 'ACTIVO' },
+    { nombre: 'BBVA', tipo: 'Transferencia bancaria', estado: 'ACTIVO' },
+    { nombre: 'Yape', tipo: 'Pago por aplicación', estado: 'ACTIVO' },
+    { nombre: 'Mercado Pago', tipo: 'Pasarela de Pago', estado: 'ACTIVO' }
   ];
 
   constructor(public dialog: MatDialog) { }
@@ -29,7 +36,14 @@ export class MetodosPagoComponent implements OnInit {
   }
 
   crearEditarMetodoPago(metodoPago?: any) {
-    this.dialog.open(CrearEditarMetodoPagoComponent);
+    this.dialog.open(CrearEditarMetodoPagoComponent, {
+      width: '27rem',
+      data: metodoPago
+    });
+  }
+
+  activarDesactivarMetodoPago(metodoPago: any) {
+
   }
 
 }
